81465be5f212dca7963d78587b11d34170829ee5
abdellatif <ouldeabdellatif40@gmail.com>
2021-02-26 16:54:02 +0100

    add code html in index

---
+++ index_.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Contact_App</title>
+</head>
+<body>
+<div class="My_web_app">
+    <div class="logout">
+        <h2>My WebApp</h2>
+         <button type="submit">logout</button>
+    </div>
+
+    <div class="contact_list">
+        <h4>Contact List :</h4>
+
+        <div class="search">
+          <form  action="" method="">
+         <input type="text" placeholder="Search.." name="search">
+         <button type="submit"><i class="fa fa-search"></i></button>
+         </form>
+        </div>
+
+        <div class="add_person">
+        <button type="submit"><a href="" > </a><i class="material-icons" ></i></button>
+        </div>
+
+    </div>
+
+    <div class="tab">
+    <table class="table">
+  
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Firstname</th>
+      <th scope="col">Lastname</th>
+      <th scope="col">Email</th>
+      <th scope="col">Adress</th>
+      <th scope="col">Phone</th>
+      <th scope="col">Group</th>
+      <th scope="col">act</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">58 011</th>
+      <td>Abby</td>
+      <td>Adnane</td>
+      <td>Abby@anywhere.com</td>
+      <td>Adress2</td>
+      <td>Phone2</td>
+      <td>Family</td>
+      <td>Edit</td>
+    </tr>
+    <tr>
+      <th scope="row">58 011</th>
+      <td>Abby</td>
+      <td>Adnane</td>
+      <td>Abby@anywhere.com</td>
+      <td>Adress3</td>
+      <td>Phone3</td>
+      <td>Family</td>
+      <td>Edit</td>
+    </tr>
+    <tr>
+      <th scope="row">58 011</th>
+      <td>Abby</td>
+      <td>Adnane</td>
+      <td>Abby@anywhere.com</td>
+      <td>Adress4</td>
+      <td>Phone4</td>
+      <td>Friend</td>
+      <td>Edit</td>
+    </tr>
+    <tr>
+      <th scope="row">58 011</th>
+      <td>Abby</td>
+      <td>Adnane</td>
+      <td>Abby@anywhere.com</td>
+      <td>Adress1</td>
+      <td>Phone1</td>
+      <td>Friend</td>
+      <td>Edit</td>
+    </tr>
+  
+  </tbody>
+</table>
+    </div>
+
+</div>
+
+</body>
+</html>
