5ee152fe6ef3dfd1b601b2fc7dc85599590bb7b9
abdellatif <ouldeabdellatif40@gmail.com>
2021-02-27 12:29:45 +0100

    add code html & css of these files

---
+++ Components/Add_Person.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Contact_App</title>
+</head>
+<body>
+<div class="add-person">
+         <div class="nav-add">
+         <h2>Add Person</h2>
+         <small class=icon></small>
+         </div>
+
+         <div class="formgroup">
+         <form action="">
+
+         <div>
+        <label for="lname">Firstname:</label><br>
+        <input type="text" id="lname" name="lname" value="Doe"><br><br>
+        </div>
+         <div>
+        <label for="lname">Lastname:</label><br>
+        <input type="text" id="lname" name="lname" value="Doe"><br><br>
+        </div>
+         <div>
+        <label for="lname">Email:</label><br>
+        <input type="email" id="lname" name="lname" value="Doe"><br><br>
+        </div>
+         <div>
+        <label for="lname">Adress:</label><br>
+        <input type="text" id="lname" name="lname" value="Doe"><br><br>
+        </div>
+         <div>
+        <label for="lname">Phone:</label><br>
+        <input type="number" id="lname" name="lname" value="Doe"><br><br>
+        </div>
+
+        <div class="choose"></div>
+
+        <div>
+        <label for="lname">notes:</label><br>
+        <input type="number" id="lname" name="lname" value="Doe"><br><br>
+        </div>
+
+        <button type="submit">Submit</button>
+
+         </form>
+         </div>
+
+     </div>
+</body>
+</html>
---
+++ Components/welcome_back.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Contact_App</title>
+</head>
+<body>
+<div class=welcome_back >
+      <div class="image"></div>
+
+       <div class="welcome">
+       <h1 class="title">Welcome back</h1>
+       <div class="login">
+         <div> <h2>Login</h2></div>
+         <form action="">
+
+         <div>  
+         <label for="fname">Email:</label><br>
+         <input type="email" class="fname" name="fname" value="enter your email"><br>
+         </div>
+
+         <div>
+         <label for="lname">Password:</label><br>
+         <input type="password" class="fname" name="lname" value=""><br><br>
+         </div>
+           
+         <div>
+         <button class="butt-login" type="submit">Login</button>
+         <button class="butt-read" type="submit">Read More</button>
+         </div>
+         </form>
+         </div>
+
+        </div>
+     </div>
+</body>
+</html>
